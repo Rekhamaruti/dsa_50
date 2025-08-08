39425a1a1e4df70586bed8f6a0f6946293b9c082
Rekha Maruti Garadimani <119103939+Rekhamaruti@users.noreply.github.com>
2025-08-08 21:44:09 +0530

    Update and rename sorted_array.py to sorted_squared_array.py

---
+++ sorted_squared_array.py
@@ -0,0 +1,7 @@
+#sorted squared array
+'''
+You are given array of integers in which each subsequent value is not less than the previous value. 
+Write a function that takes this array as an input and returns a new array with the squares of 
+each number sorted in ascending order
+'''
+
