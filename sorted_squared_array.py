3f2838568386cbe3d349714b702cca7d9e952592
Rekha Maruti Garadimani <119103939+Rekhamaruti@users.noreply.github.com>
2025-08-17 19:11:49 +0530

    Update sorted_squared_array.py

--- sorted_squared_array.py
+++ sorted_squared_array.py
@@ -1,7 +1,14 @@
-#sorted squared array
+#sorted squared array - Brute force method
 '''
 You are given array of integers in which each subsequent value is not less than the previous value. 
 Write a function that takes this array as an input and returns a new array with the squares of 
 each number sorted in ascending order
 '''
+def squared_sorted(array):
+  n =len(array)
+  res = []*n
+  for i in range(n):
+    res[i]=array[i]**2
+  res.sort()
+  return res
 
