ef3c999f940d46773285b19f514eeb5a9954c41d
Rekha Maruti Garadimani <119103939+Rekhamaruti@users.noreply.github.com>
2025-08-17 19:36:31 +0530

    Update sorted_squared_m2.py

--- sorted_squared_m2.py
+++ sorted_squared_m2.py
@@ -1,1 +1,13 @@
-
+def sorted_squared(array):
+  n = len(array)
+  i = 0
+  j = n - 1
+  res = [0]*n
+  for k in reversed(range(n)):
+    if array[i]**2 > array[j]**2:
+      res[k]=array[i]**2
+      i+=1
+    else:
+      res[k]=array[j]**2
+      j-=1
+  return res
