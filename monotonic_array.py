23351c2455347c695a245dca52c34d44f5910566
Rekha Maruti Garadimani <119103939+Rekhamaruti@users.noreply.github.com>
2025-08-17 20:50:11 +0530

    Create monotonic_array.py

---
+++ monotonic_array.py
@@ -0,0 +1,17 @@
+#Monotonic array
+def monotonic_array(array):
+  n = len(array)
+  if n == 0:return False #if the given array is empty
+  first = array[0]
+  last = array[n-1]
+  if first > last:
+    #monotonic decreasing
+    for i in range(n-1):
+      if array[i]<=array[i+1]:return False
+  elif first == last:
+    for i in range(n-1):
+      if array[i]!=array[i+1]:return False
+  else:
+    for i in range(n-1):
+      return False
+  return True
